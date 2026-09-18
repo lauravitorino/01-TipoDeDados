0d9646387b7567a6835ba6c1054c9d2e0bb8b46d
Laura Vitorino Carvalho <garotael17@gmail.com>
2026-09-17 21:29:32 -0300

    Update TiposDeDados.cpp
    Exercicíos 1,2,3,4

--- 1_TipoDados/TiposDeDados/TiposDeDados/TiposDeDados.cpp
+++ 1_TipoDados/TiposDeDados/TiposDeDados/TiposDeDados.cpp
@@ -48,36 +48,29 @@ int main()
 
 	cout << endl << endl;
 
-	//cout << "Qualquer tecla para continuar...";
-	//system("pause"); // somente no Windows
-	//system("cls");
-
-	//cout << "Entrada de Dados" << endl;
-	//cout << "================" << endl;
-	//string nome = "";
-	//int idade = 0;
-	//int dias = 0;
-	//int horas = 0;
-	//int minutos = 0;
-
-	//cout << "Digite seu nome: ";
-	//cin >> nome;
-	//cout << "Digite sua idade: ";
-	//cin >> idade;
-	//dias = idade * 365;
-	//horas = dias * 24;
-	//minutos = horas  * 60;
-	//cout << "Sua idade em dias e: " << dias << endl;
-	//cout << "Sua idade em horas e: " << horas << endl;
-	//cout << "Sua idade em minutos e: " << minutos << endl;
-
 	cout << "Qualquer tecla para continuar...";
 	system("pause"); // somente no Windows
 	system("cls");
 
+	//Exercicío 1
+	#include <climits>
+	cout << "int  max = " << INT_MAX  << "\n";
+	cout << "int  min = " << INT_MIN  << "\n";
+	cout << "short max = " << SHRT_MAX << "\n";
 
-	//Exercic�o 2
-	cout << "Convers�o de Temperatura" << endl;
+	int x = INT_MAX;
+    x = x + 1;
+    cout << "INT_MAX + 1 = " << x << "\n";
+
+    return 0;
+
+
+	cout << "Qualquer tecla para continuar...";
+	system("pause"); // somente no Windows
+	system("cls");
+	
+	//Exercicío 2
+	cout << "Conversão de Temperatura" << endl;
 	int celsius = 0;
 	double fahrenheit = 0;
 	double kelvin = 0;
@@ -94,7 +87,7 @@ int main()
 	system("pause"); // somente no Windows
 	system("cls");
 
-	//Exercic�o 3
+	//Exercicío 3
 	cout << "Calculadora de Segundos" << endl;
 	long segundos = 0;
 	int dias = 0;
@@ -115,13 +108,24 @@ int main()
 	cout << "A qtd de minutos e: " << horas << endl;
 	cout << "A qtd de dias e: " <<  dias << endl;
 	cout <<"A qtd de segundos restantes e: " << segundosR << endl;
+
+	cout << "Qualquer tecla para continuar...";
+	system("pause"); // somente no Windows
+	system("cls");
 	
+	//Exercicio 4
+	cout << "Ponteiro na Prática" << endl;
+	int var = 10; /* a casa*/
 
-
-		
-
-
-
+	cout << "Valor: " << var << endl;
+	cout << &var << endl; /* o & seria o endereço da casa */
+	cout << "Endereço: " << var << endl;
+	
+	int* ponteiro = &numero; /* e o pnteiro é o caminho q segue o endereço ate a casa*/
+	*ponteiro = *ponteiro * 2;
+	
+	cout << "Novo valor: " << numero << endl;
+	
 	cout << "Qualquer tecla para sair...";
 	system("pause"); // somente no Windows
 
